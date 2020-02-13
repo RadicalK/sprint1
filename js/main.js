b0298dea25bd4dee0a0c649caeaa6ae1f3fdfb38
test-ichikawa <i100057g@gmail.com>
2020-02-13 15:34:55 +0900

    commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
-$('#sound').on('click', function() {
+$('#sound1').on('click', function() {
 	$( '#sound-file' ).get(0).play() ;
-})
+});
+
+$('#sound2').on('click', function() {
+	$( '#sound-file2' ).get(0).play() ;
+});
+
+$('#sound3').on('click', function() {
+	$( '#sound-file3' ).get(0).play() ;
+});
