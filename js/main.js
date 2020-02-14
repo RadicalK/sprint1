2f1e53ede8ae9e599f286d7d0fa7b8905033744a
test-ichikawa <i100057g@gmail.com>
2020-02-14 11:19:40 +0900

    commit

--- js/main.js
+++ js/main.js
@@ -13,8 +13,25 @@ $('#sound3').on('click', function() {
 var aX = 0,
 	aY = 0,
 	aZ = 0; // 加速度の値を入れる変数を3個用意
+var oldY = 0;
 
-	var oldY = 0;
+$('#permission').on('click', function() {
+	ClickRequestDeviceSensor();
+})
+function ClickRequestDeviceSensor(){
+  //. ユーザーに「許可」を求めるダイアログを表示
+  DeviceMotionEvent.requestPermission().then( function( response ){
+    if( response === 'granted' ){
+			window.addEventListener("devicemotion", (dat) => {
+				aX = dat.accelerationIncludingGravity.x;
+				aY = dat.accelerationIncludingGravity.y;
+				aZ = dat.accelerationIncludingGravity.z;
+			});
+    }
+  }).catch( function( e ){
+    console.log( e );
+  });
+}
 
 // 加速度センサの値が変化したら実行される devicemotion イベント
 window.addEventListener("devicemotion", (dat) => {
@@ -30,12 +47,13 @@ var timer = window.setInterval(() => {
 // データを表示する displayData 関数
 function displayData() {
 	var txt = document.getElementById("txt");
-	// txt.innerHTML = "x: " + aX + "<br>" // x軸の値
-	// 	+
-	// 	"y: " + aY + "<br>" // y軸の値
-	// 	+
-	// 	"z: " + aZ; // z軸の値
-		txt.innerHTML = 'v3 ' + 'yの差分 ' + Math.abs(aY - oldY);
+	txt.innerHTML = "x: " + aX + "<br>" // x軸の値
+		+
+		"y: " + aY + "<br>" // y軸の値
+		+
+		"z: " + aZ + "<br>"  // z軸の値
+		+
+		'v4 ' + 'yの差分 ' + Math.abs(aY - oldY);
 
 		if(aY - oldY > 2 && oldY !== 0) {
 			$( '#sound-file3' ).get(0).play() ;
